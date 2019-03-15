12e8121f0060087f3db7a2f976a67428e7711495
ybabu <ybabu@r1rcm.com>
2019-03-15 17:26:39 +0530

    Initial commit

---
+++ TestBOM/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Runtime.Serialization;
+namespace TestBOM
+{
+    class Program
+    {
+ 
+        static void Main(string[] args)
+        {
+
+            var PatientContainer = TestVist.InitializeConnection();
+            var visitInfo = PatientContainer.CreateQuery<TestBOM.Visit>("Visit").Execute().ToList();
+
+
+        }
+    }
+}
---
+++ TestBOM/TestVist.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestBOM
+{
+    public class TestVist
+    {
+        public static TestBOM.DefaultContainer InitializeConnection()
+        {
+            TestBOM.DefaultContainer VisitContainer;
+            VisitContainer = new TestBOM.DefaultContainer(new Uri("http://alppatdevweb04.accretivehealth.local:5555/data/VisitService/v2.0"))
+            {
+                Credentials = new NetworkCredential("Administrator", "manage")
+            };
+
+          
+            VisitContainer.BuildingRequest += (s, e) =>
+            {
+                e.Headers.Add("facilityCode", "BOMC");
+                e.Headers.Add("Authorization", "Basic QWRtaW5pc3RyYXRvcjptYW5hZ2U=");
+            };
+
+            return VisitContainer;
+        }
+    }
+}
